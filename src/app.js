3db434a9eb464eb742b7241bdcf1c01dfbe3ef05
Emon8794 <emonabujafor@gmail.com>
2024-02-12 22:46:08 +0600

    handle error, response and start server

--- src/app.js
+++ src/app.js
@@ -1,6 +1,17 @@
+// Dependencies
+import cookieParser from "cookie-parser";
+import cors from "cors";
 import express from "express";
 
+// App Initialization
 const app = express();
 
+// Middlewares
+app.use(express.json({ limit: "20kb" }));
+app.use(express.urlencoded({ limit: "20kb" }));
 // Anything in this directory will be served up as static content.
 app.use(express.static("public"));
+app.use(cors({ credentials: true, origin: process.env.CORS_ORIGIN }));
+app.use(cookieParser());
+
+export default app;
--- src/index.js
+++ src/index.js
@@ -1,9 +1,26 @@
 // Dependencies
 import dotenv from "dotenv";
+import app from "./app.js";
 import connectDB from "./db/index.js";
-
-// require("dotenv").config({path: "./env"})
+// configure environment variables
+dotenv.config({ path: "./env" });
 
 // configuration
-dotenv.config({ path: "./env" });
-connectDB();
+const { PORT } = process.env;
+
+// connect to DB and start server
+connectDB()
+  .then(() => {
+    // check if app is ready to run
+    app.on("error", (error) => {
+      console.log("Application isn't ready to run");
+      throw error;
+    });
+    // start server
+    app.listen(PORT, () => {
+      console.log(`Server is running on port ${PORT}`);
+    });
+  })
+  .catch((err) => {
+    console.log("MongoDB connection failed !!!", err);
+  });
